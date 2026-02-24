67761a6b49e3d3213baa2e09897b0bb120d27f48
Wasid Ansari <114327679+Wasid786@users.noreply.github.com>
2026-02-24 21:58:04 +0530

    delete feature added

--- mark01/mark01/urls.py
+++ mark01/mark01/urls.py
@@ -19,7 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('tweet/', include('tweet.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
